f7514e9933a2dd6641418f5edc37876508711f00
Alazar-dev <mealazartadesse@gmail.com>
2021-10-06 20:11:26 +0300

    api env changed

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://app-546f7240-23f5-4dea-b833-661ef1d607a3.cleverapps.io/";
+  "https://app-546f7240-23f5-4dea-b833-661ef1d607a3.cleverapps.io/api";
 // const API_LOCAL = "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
