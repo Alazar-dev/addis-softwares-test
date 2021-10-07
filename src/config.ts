2682ff5078566b61a5c10acaa0dcd3bb22248206
Alazar-dev <mealazartadesse@gmail.com>
2021-10-07 16:15:21 +0300

    Remote url

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const API_URL =
-//   "https://app-546f7240-23f5-4dea-b833-661ef1d607a3.cleverapps.io/api";
-const API_URL = "http://localhost:3000/api";
+const API_URL =
+  "https://app-546f7240-23f5-4dea-b833-661ef1d607a3.cleverapps.io/api";
+// const API_URL = "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_URL,
